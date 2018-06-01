d184a502852089d15149e8f81e7878b6cbbe2b1a
Jo Malo <juanitamalo.p@gmail.com>
2018-06-01 14:41:31 +0100

    item abstract class added

--- src/main/java/film_memorabilia/Item.java
+++ src/main/java/film_memorabilia/Item.java
@@ -1,4 +1,29 @@
 package film_memorabilia;
 
-public class Item {
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public abstract class Item {
+    protected int buyPrice;
+    protected int shipping;
+    protected int marketValue;
+
+    public Item(int buyPrice, int shipping, int marketValue) {
+        this.buyPrice = buyPrice;
+        this.shipping = shipping;
+        this.marketValue = marketValue;
+    }
+
+    public int getBuyPrice() {
+        return this.buyPrice;
+    }
+
+    public int getShipping() {
+        return this.shipping;
+    }
+
+    public int getMarketValue() {
+        return this.marketValue;
+    }
+
 }
